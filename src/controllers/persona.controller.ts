cb063d252e75a62cd13ac9d3fc46954cc14ef86f
felex83 <felipeandresvelez@gmail.com>
2021-12-09 18:57:24 -0500

    Actualizacion

--- src/controllers/persona.controller.ts
+++ src/controllers/persona.controller.ts
@@ -1,30 +1,57 @@
+import {service} from '@loopback/core';
 import {
   Count,
   CountSchema,
   Filter,
   FilterExcludingWhere,
   repository,
-  Where,
+  Where
 } from '@loopback/repository';
 import {
-  post,
-  param,
-  get,
-  getModelSchemaRef,
-  patch,
-  put,
-  del,
-  requestBody,
-  response,
+  del, get,
+  getModelSchemaRef, HttpErrors, param, patch, post, put, requestBody,
+  response
 } from '@loopback/rest';
-import {Persona} from '../models';
+import { Llaves } from '../config/llaves';
+import {Credenciales, Persona} from '../models';
 import {PersonaRepository} from '../repositories';
+import {AutenticacionService} from '../services';
+const fetch = require("node-fetch");
 
 export class PersonaController {
   constructor(
     @repository(PersonaRepository)
     public personaRepository : PersonaRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion : AutenticacionService,
   ) {}
+
+@post("/identificarPersona",{
+  responses: {
+    '200':{
+      description: "Identificacion de usuarios"
+    }
+  }
+})
+async identificarPersona(
+  @requestBody() credenciales: Credenciales
+) {
+  let p = await this.servicioAutenticacion.IdentificarPersonas(credenciales.usuario, credenciales.clave);
+  if(p){
+    let token = this.servicioAutenticacion.GenerarTokenJWT(p);
+    return{
+      datos: {
+        nombres: p.nombres,
+        correo: p.correo,
+        id: p.id
+      },
+      tk: token
+    }
+  }else{
+    throw new HttpErrors[401]("Datos invalidos");
+  }
+}
+    
 
   @post('/personas')
   @response(200, {
@@ -44,7 +71,24 @@ export class PersonaController {
     })
     persona: Omit<Persona, 'id'>,
   ): Promise<Persona> {
-    return this.personaRepository.create(persona);
+
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    persona.clave = claveCifrada;
+    let p = await this.personaRepository.create(persona);
+
+    //Notificar al usuario para
+    let destino = persona.correo;
+    let asunto = "Registro en la PLataforma";
+    let contenido = `Hola ${persona.nombres}, su nombre de usuario es: ${persona.correo} y su contraseña es: ${clave}.`;
+    fetch(`${Llaves.urlServcioNotificationes}/envio-correo?correo_destino=${destino}&asunto=${asunto}&contenido=${contenido}`)
+    .then((data: any) => {
+      console.log(data);
+    })
+    return p;
+
+
+
   }
 
   @get('/personas/count')
